103244d4e7eee154d1b882cbd49ee9c8779024c5
CHORONGE <leewnsdud123@gmail.com>
2023-12-03 17:59:56 +0900

    DB connection

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017')
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+const {Schema} = mongoose;
+
+const WritingSchema = new Schema({
+  title: String,
+  contents: String,
+  date: {
+    type: Date,
+    default: Date.now,
+  }
+})
+
+const Writing = mongoose.model('Writing', WritingSchema);
+
 module.exports = router;

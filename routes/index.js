41023989db55710549e14d7b525067a925f04dbe
CHORONGE <leewnsdud123@gmail.com>
2023-12-04 03:20:14 +0900

    register module added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -10,18 +9,5 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017')
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
-
-const {Schema} = mongoose;
-
-const WritingSchema = new Schema({
-  title: String,
-  contents: String,
-  date: {
-    type: Date,
-    default: Date.now,
-  }
-})
-
-const Writing = mongoose.model('Writing', WritingSchema);
 
 module.exports = router;
